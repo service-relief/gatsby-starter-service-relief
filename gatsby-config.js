98e175fdc30fca2cd1bee026967a8e2f23a44b74
service-relief <johno-actions-push-subdirectories@example.org>
2020-03-20 17:14:16 +0000

    Update gatsby-starter-service-relief from service-relief/service-relief

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,8 +5,6 @@ module.exports = {
       options: {
         authorName: `@amber1ey`,
         authorLink: `https://twitter.com/amber1ey`,
-        city: `Austin`,
-        state: `Texas`,
       },
     },
   ],
